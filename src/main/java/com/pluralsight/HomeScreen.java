fac8f2b96c97dba661361f6afe641cd9ea09405d
Alexander Leal <computerlanguage5280@gmail.com>
2025-05-22 15:37:40 -0500

    completed basic HomeScreen, added diagram

--- src/main/java/com/pluralsight/HomeScreen.java
+++ src/main/java/com/pluralsight/HomeScreen.java
@@ -5,19 +5,26 @@ import java.util.Scanner;
 public class HomeScreen {
     Scanner scanner = new Scanner(System.in);
 
-
+    boolean input = true;
 
     public HomeScreen(){
         boolean homeSelect = true;
         while (homeSelect){
-            System.out.println("##==Welcome to DELIcious!==##");
+            System.out.println("##===Welcome to DELIcious!===##");
             System.out.println("Please select an option below: ");
             System.out.println("1) New Order");
             System.out.println("0) Exit");
-            scanner.nextLine();
 
+            String choice = scanner.nextLine();
 
-        break;
+            if(choice.equals("1")){
+                System.out.println("New Order -->");
+            } else if (choice.equals("0")) {
+                System.out.println("Exiting the application... Goodbye!");
+                homeSelect = false;
+            }else {
+                System.out.println("Invalid input. Please enter only 1 or 0.");
+            }
         }
     }
 }
