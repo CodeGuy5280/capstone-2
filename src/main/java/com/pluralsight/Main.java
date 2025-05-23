a036ac7e60d5755c58d7e498f07633475017bb74
CodeGuy5280 <computerlanguage5280@gmail.com>
2025-05-23 11:29:28 -0500

    updating OrderScreen

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -5,5 +5,6 @@ public class Main {
 
 
         HomeScreen homeScreen = new HomeScreen();
+        OrderScreen newOrder = new OrderScreen();
     }
 }
--- src/main/java/com/pluralsight/OrderScreen.java
+++ src/main/java/com/pluralsight/OrderScreen.java
@@ -7,28 +7,34 @@ public class OrderScreen {
 
 
     public void newOrder(){
-         OrderScreen newOrder = new OrderScreen();
-        System.out.println("--- New Order ---");
 
-        String choice = scanner.nextLine();
-        switch (choice){
-             case "1":
-                 System.out.println("1) Add Sandwich");
-                 break;
-             case "2":
-                 System.out.println("2) Add Drink");
-                 break;
-             case "3":
-                 System.out.println("3) Add Chips");
-                 break;
-             case "4":
-                 System.out.println("4) Checkout");
-                 break;
-             case "5":
-                 System.out.println("0) Cancel Order");
-                 break;
-             default:
-                 System.out.println("Please select a valid option.");
-         }
+        boolean orderInProgress = true;
+        while(orderInProgress){
+            System.out.println("\n--- New Order ---");
+            System.out.println("1) Add Sandwich");
+            System.out.println("2) Add Drink");
+            System.out.println("3) Add Chips");
+            System.out.println("4) Checkout");
+            System.out.println("0) Cancel Order");
+            System.out.println("Enter your choice: ");
+
+
+            String choice = scanner.nextLine();
+
+            switch (choice) {
+            case "1":
+                break;
+            case "2":
+                break;
+            case "3":
+                break;
+            case "4":
+                break;
+            case "5":
+                break;
+            default:
+                System.out.println("Please select a valid option.");
+        }
+        }
     }
 }
